b7f22a222b782dd0c9631e47fbeb57f240c34a12
nkizito92 <nkizito57@gmail.com>
2020-04-01 15:16:46 -0500

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,11 +1,9 @@
-// Code EyesOnMe Component Here
-import React, { Component } from 'react';
 
-export default class EyesOnMe extends Component {
+import React, { Component } from 'react'
+
+export default class Keypad extends Component {
     render() {
-        return (
-            <button> Click me!!<button />
-      )
-  }
-  
+        return <input type="password" />
+
+    }
 }
