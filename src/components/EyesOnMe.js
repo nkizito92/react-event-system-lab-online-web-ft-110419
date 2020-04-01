7a31f4f06b55a62aec25cf4ba7940dec3629ebfa
nkizito92 <nkizito57@gmail.com>
2020-04-01 15:16:29 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,11 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+    render() {
+        return (
+            <button> Click me!!<button />
+      )
+  }
+  
+}
