d490639e8325b52940b0997697df5bdb19a4110a
Kizito Njoku <nkizito57@gmail.com>
2020-04-02 01:44:47 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,11 @@
 
 import React, { Component } from 'react'
-
+  handleKeyUp = (e) => {
+    return constol.log(this.e.target)
+  }
 export default class Keypad extends Component {
     render() {
-        return <input onKeyUp="" type="password" />
+        return <input onKeyUp={this.handleKeyUp} type="password" />
 
     }
 }
